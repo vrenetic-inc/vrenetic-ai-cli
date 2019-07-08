8bc12f233a35389d1ea8073cb7e062259b02ad5b
Bart Jakubowski <bart@jakubowski.in>
2019-07-08 16:42:11 +0200

    Trying to fix setup

--- setup.py
+++ setup.py
@@ -14,4 +14,8 @@ except VersionConflict:
 
 
 if __name__ == "__main__":
-    setup(use_pyscaffold=True)
+    setup(
+        use_pyscaffold=True,
+        long_description=long_description,
+        long_description_content_type='text/markdown'
+    )
