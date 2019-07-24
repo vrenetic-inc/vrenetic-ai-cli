3722d3a420e1e5862cd3b4fbd4cbdb5fae686ec3
kris <krzysztof.piotr.stasiak@gmail.com>
2019-07-24 17:21:38 +0200

    added tensorflow backend

--- src/vrenetic/providers/application.py
+++ src/vrenetic/providers/application.py
@@ -21,6 +21,14 @@ def info(version):
     except:
         print('PyTorch support: NO')
 
+
+    try:
+        has_tensorflow()
+        print('TensorFlow support: YES')
+    except:
+        print('TensorFlow support: NO')
+
+
     try:
         print()
         print('------- OpenCL details -------')
@@ -39,6 +47,13 @@ def info(version):
         print()
         print('------- PyTorch details -------')
         pytorch_details()
+    except Exception as error:
+        print(error)
+
+    try:
+        print()
+        print('------- TensorFlow details -------')
+        tensorflow_details()
     except Exception as error:
         print(error)
 
@@ -77,3 +92,15 @@ def has_pytorch():
 def pytorch_details():
     from backends import pytorch
     pytorch.details()
+
+
+def has_tensorflow():
+    try:
+        from backends import tensorflow
+    except:
+        raise ImportError('Cannot load TensorFlow support')
+
+def tensorflow_details():
+    from backends import tensorflow
+    tensorflow.details()
+
